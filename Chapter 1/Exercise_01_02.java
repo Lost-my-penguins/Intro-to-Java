430941e4a086ab441e85141b1da9f498164a58ae
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-29 15:33:12 -0700

    Update and rename Exercise.java to Exercise_01_02.java

--- Chapter 1/Exercise_01_02.java
+++ Chapter 1/Exercise_01_02.java
@@ -1,6 +1,6 @@
 // (Display five messages) Write a program that displays I love Java five times
 
-public class Exercise {
+public class Exercise_01_02 {
 	public static void main(String[] args) {
 		System.out.println("I love Java");
 		System.out.println("I love Java");
