4ea7510631316b0940e76febeb5de89d5563e409
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-29 16:07:08 -0700

    Update Exercise_01_05.java

--- Chapter 1/Exercise_01_05.java
+++ Chapter 1/Exercise_01_05.java
@@ -4,6 +4,6 @@
 
 public class Exercise_01_05 {
 	public static void main(String[] args) {
-		System.out.println((7.5*6.5-4.5*3)/(47.5-5.5));
+		System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
 	}
 }
