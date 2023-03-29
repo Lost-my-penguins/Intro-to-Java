19df0ef6f87dbcd3eed7c45f6a619a1016b472a4
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-29 15:32:39 -0700

    Add files via upload

---
+++ Chapter 1/Exercise.java
@@ -0,0 +1,11 @@
+// (Display five messages) Write a program that displays I love Java five times
+
+public class Exercise {
+	public static void main(String[] args) {
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+	}
+}
