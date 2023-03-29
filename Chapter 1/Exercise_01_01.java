4134cfd0c64d4e787718ada9be3021298992c120
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-29 15:26:05 -0700

    Update Exercise_01_01.java

--- Chapter 1/Exercise_01_01.java
+++ Chapter 1/Exercise_01_01.java
@@ -6,8 +6,8 @@
 
 public class Exercise_01_01 {
 	public static void main(String[] args) {
-				System.out.println("Welcome to Java");
-				System.out.println("Learning Java Now");
-				System.out.println("Programming is Fun");
+		System.out.println("Welcome to Java");
+		System.out.println("Learning Java Now");
+		System.out.println("Programming is Fun");
 	}
 }
