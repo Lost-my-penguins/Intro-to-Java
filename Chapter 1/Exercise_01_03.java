49c35b22d1243bd0c07e5271534381d5a09856da
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-29 15:41:20 -0700

    Update and rename Exercise.java to Exercise_01_03.java

--- Chapter 1/Exercise_01_03.java
+++ Chapter 1/Exercise_01_03.java
@@ -5,7 +5,7 @@
  *	  J J   A     A    V    A     A
  */
 
-public class Exercise {
+public class Exercise_01_03 {
 	public static void main(String[] args) {
 		System.out.println("    J     A    V     V    A");
 		System.out.println("    J    A A    V   V    A A");
