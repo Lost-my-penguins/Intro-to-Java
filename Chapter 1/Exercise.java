12205722648f0bbb9af1f446dd6efd027bf6a5d3
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:12:20 -0700

    Add files via upload

---
+++ Chapter 1/Exercise.java
@@ -0,0 +1,15 @@
+/* (Approximate π) π can be computed using the following formula:
+ * 		π = 4*(1-1/3+1/5-1/7+1/9-1/11+...)
+ *  Write a program that displays the result of 
+ *  	π = 4*(1-1/3+1/5-1/7+1/9-1/11)
+ *  and
+ *  	π = 4*(1-1/3+1/5-1/7+1/9-1/11+1/13)
+ * Use 1.0 instead of 1 in your program
+ */
+
+public class Exercise {
+	public static void main(String[] args) {
+		System.out.println(4 * (1.0 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11));
+		System.out.println(4 * (1.0 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13));
+	}
+}
---
+++ Chapter 1/Exercise2.java
@@ -0,0 +1,13 @@
+/* (Area and perimeter of a rectangle) Write a program that displays
+ * that area and perimeter of a circle using the following formula:
+ * 		π = 3.14159
+ * 		perimeter = 2*radius*π
+ * 		area = radius*radius*π
+ */
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Perimeter = " + 2 * 6.5 * 3.14159);
+		System.out.println("Area = " + 6.5 * 6.5 * 3.14159);
+	}
+}
