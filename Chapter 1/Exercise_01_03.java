a26829f0666d92d51f2e793b0384fbdc90eeadae
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:16:04 -0700

    Update Exercise_01_03.java

--- Chapter 1/Exercise_01_03.java
+++ Chapter 1/Exercise_01_03.java
@@ -1,6 +1,6 @@
 /* (Display a pattern) Write a program that displays the following pattern
  *       J     A    V     V    A
- *	 J    A A    V   V    A A
+ *	 	 J    A A    V   V    A A
  *   J   J   AAAAA    V V    AAAAA
  *    J J   A     A    V    A     A
  */
