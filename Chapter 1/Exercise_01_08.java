226947a621848d9c0625554228c3d02a89989a69
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:14:13 -0700

    Update and rename Exercise2.java to Exercise_01_08.java

--- Chapter 1/Exercise_01_08.java
+++ Chapter 1/Exercise_01_08.java
@@ -1,11 +1,11 @@
 /* (Area and perimeter of a rectangle) Write a program that displays
  * that area and perimeter of a circle using the following formula:
- * 		π = 3.14159
- * 		perimeter = 2*radius*π
- * 		area = radius*radius*π
+ * 	π = 3.14159
+ * 	perimeter = 2*radius*π
+ *	area = radius*radius*π
  */
 
-public class Exercise2 {
+public class Exercise_01_08 {
 	public static void main(String[] args) {
 		System.out.println("Perimeter = " + 2 * 6.5 * 3.14159);
 		System.out.println("Area = " + 6.5 * 6.5 * 3.14159);
