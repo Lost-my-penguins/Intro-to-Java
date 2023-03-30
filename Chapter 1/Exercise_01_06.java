15e3f2c70117e9e37596d5118859caca7c9bcf03
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:04:04 -0700

    Update and rename Exercise.java to Exercise_01_06.java

--- Chapter 1/Exercise_01_06.java
+++ Chapter 1/Exercise_01_06.java
@@ -1,8 +1,8 @@
 /* (Summation of a series) Write a program that displays the result of
- * 		1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10
+ * 	1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10
  */
 
-public class Exercise {
+public class Exercise_01_06 {
 	public static void main(String[] args) {
 		System.out.println(1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10);
 	}
