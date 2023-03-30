44b549b20d06ab8aaf5c6689e9ffb5944193660c
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:03:28 -0700

    Add files via upload

---
+++ Chapter 1/Exercise.java
@@ -0,0 +1,9 @@
+/* (Summation of a series) Write a program that displays the result of
+ * 		1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10
+ */
+
+public class Exercise {
+	public static void main(String[] args) {
+		System.out.println(1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10);
+	}
+}
