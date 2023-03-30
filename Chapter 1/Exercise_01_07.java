01251ecac9c2819f24d58aded8e67bee081c577e
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-30 16:19:01 -0700

    Update Exercise_01_07.java

--- Chapter 1/Exercise_01_07.java
+++ Chapter 1/Exercise_01_07.java
@@ -4,7 +4,7 @@
  *  	π = 4*(1-1/3+1/5-1/7+1/9-1/11)
  *  and
  *  	π = 4*(1-1/3+1/5-1/7+1/9-1/11+1/13)
- * Use 1.0 instead of 1 in your program
+ *  Use 1.0 instead of 1 in your program
  */
 
 public class Exercise_01_07 {
