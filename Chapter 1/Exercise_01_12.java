cd01f5abf8a83981b583610d474a7728f8d66bb2
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-31 10:37:11 -0700

    Update and rename Exercise2.java to Exercise_01_12.java

--- Chapter 1/Exercise_01_12.java
+++ Chapter 1/Exercise_01_12.java
@@ -5,7 +5,7 @@
  *  (Note 1 mile is equal to 1.6 kilometers)
  */
 
-public class Exercise2 {
+public class Exercise_01_12 {
 	public static void main(String[] args) {
 		System.out.println("Average speed of runner is: " + ((24 * 1.6)/(1 + (40.0/60) + (35.0/3600))));
 	}
