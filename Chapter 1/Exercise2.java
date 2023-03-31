31fccdbe8c2ce137a733dca661aa0b012502e13e
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-31 10:05:30 -0700

    Add files via upload

---
+++ Chapter 1/Exercise2.java
@@ -0,0 +1,11 @@
+/* (Average speed in miles) Assume that a runner runs 15 kilometers
+ * in 50 minutes and 30 seconds. Write a program that displays the average
+ * speed in miles per hour. 
+ * (Note that 1 mile is 1.6 kilometers)
+ */
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Average speed of the runner is: " + (15/1.6)/(50.5/60));
+	}
+}
