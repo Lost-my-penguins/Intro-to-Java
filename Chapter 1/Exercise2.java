40f0ff76b1f9b8561ae2ee3b9db07ac6ddfbaaec
Lost my Penguins <114039567+Lost-my-penguins@users.noreply.github.com>
2023-03-31 10:36:23 -0700

    Add files via upload

---
+++ Chapter 1/Exercise2.java
@@ -0,0 +1,12 @@
+/* (Average speed in kilometers) Assume that a runner runs
+ *  24 miles in 1 hours, 40 minutes, and 35 seconds.vWrite 
+ *  a program that displays the average speed in kilometers 
+ *  per hour.
+ *  (Note 1 mile is equal to 1.6 kilometers)
+ */
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Average speed of runner is: " + ((24 * 1.6)/(1 + (40.0/60) + (35.0/3600))));
+	}
+}
